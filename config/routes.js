3ddb0d57b047307c468970adf4aa12178f7dd1e3
Pablo Laso Cortabitarte <pablo.lasocortabitarte@telefonica.com>
2021-02-03 17:37:19 +0100

    register

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
 const miscController = require('../controllers/misc.controller')
+const usersController = require('../controllers/users.controller')
 
 // Misc
 
 router.get('/', miscController.home)
+
+// Users
+
+router.get('/register', usersController.register)
+router.post('/register', usersController.doRegister)
 
 module.exports = router;
