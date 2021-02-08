d16f29cc69dbfd0b6ec12f5debcee6c523d9336c
Carol Alonso <c.alonsolp@gmail.com>
2021-02-08 23:05:01 +0100

    Add email address verification with Nodemailer

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,78 +1,101 @@
-const mongoose = require("mongoose")
-const User = require("../models/user.model")
+const mongoose = require("mongoose");
+const User = require("../models/user.model");
+const { sendActivationEmail } = require("../config/mailer.config");
 
 module.exports.register = (req, res, next) => {
-  res.render('users/register')
-}
+  res.render("users/register");
+};
 
 module.exports.doRegister = (req, res, next) => {
   function renderWithErrors(errors) {
-    res.status(400).render('users/register', {
+    res.status(400).render("users/register", {
       errors: errors,
-      user: req.body
-    })
+      user: req.body,
+    });
   }
 
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (user) {
         renderWithErrors({
-          email: 'Ya existe un usuario con este email'
-        })
+          email: "Ya existe un usuario con este email",
+        });
       } else {
         User.create(req.body)
-          .then(() => {
-            res.redirect('/')
+          .then((u) => {
+            sendActivationEmail(u.email, u.activationToken);
+            res.redirect("/");
           })
-          .catch(e => {
+          .catch((e) => {
             if (e instanceof mongoose.Error.ValidationError) {
-              renderWithErrors(e.errors)
+              renderWithErrors(e.errors);
             } else {
-              next(e)
+              next(e);
             }
-          })
+          });
       }
     })
-    .catch(e => next(e))
-}
+    .catch((e) => next(e));
+};
 
 module.exports.login = (req, res, next) => {
-  res.render('users/login')
-}
+  res.render("users/login");
+};
 
 module.exports.doLogin = (req, res, next) => {
-  function renderWithErrors() {
-    res.render('users/login', {
+  function renderWithErrors(e) {
+    res.render("users/login", {
       user: req.body,
-      error: 'El correo electrónico o la contraseña no son correctos'
-    })
+      error: e || "El correo electrónico o la contraseña no son correctos",
+    });
   }
 
   User.findOne({ email: req.body.email })
     .then((user) => {
       if (!user) {
-        renderWithErrors()
+        renderWithErrors();
       } else {
-        user.checkPassword(req.body.password)
-          .then(match => {
-            if (match) {
-              req.session.currentUserId = user.id
+        user.checkPassword(req.body.password).then((match) => {
+          if (match) {
+            if (user.active) {
+              req.session.currentUserId = user.id;
 
-              res.redirect('/profile')
+              res.redirect("/profile");
             } else {
-              renderWithErrors()
+              renderWithErrors("Tu cuenta no está activa, mira tu email");
             }
-          })
+          } else {
+            renderWithErrors();
+          }
+        });
       }
     })
-    .catch(e => next(e))
-}
+    .catch((e) => next(e));
+};
 
 module.exports.logout = (req, res, next) => {
-  req.session.destroy()
-  res.redirect('/')
-}
+  req.session.destroy();
+  res.redirect("/");
+};
 
 module.exports.profile = (req, res, next) => {
-  res.render('users/profile')
-}
+  res.render("users/profile");
+};
+
+module.exports.activate = (req, res, next) => {
+  User.findOneAndUpdate(
+    { activationToken: req.params.token, active: false },
+    { active: true, activationToken: "active" }
+  )
+    .then((u) => {
+      if (u) {
+        res.render("users/login", {
+          user: req.body,
+          message: "Felicidades, has activado tu cuenta. Ya puedes iniciar sesión",
+        });
+      } else {
+        res.redirect("/")
+      }
+    })
+    .catch((e) => next(e));
+};
